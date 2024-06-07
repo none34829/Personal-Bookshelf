89581bd49c39fdcce3284884fe96a967a44e0d93
none34829 <133172306+none34829@users.noreply.github.com>
2024-06-08 04:23:35 +0530

    navbar changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,10 @@ import './Navbar.css';
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-brand">
-        <Link to="/">Book App</Link>
-      </div>
       <ul className="navbar-nav">
+        <li className="navbar-brand">
+          <Link to="/">Book App</Link>
+        </li>
         <li>
           <Link to="/">Book Search</Link>
         </li>
